f6741a3f06fcebfba098584c93985e3dbc87d8d7
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2017-03-22 16:13:10 +1300

    v2.0.4

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-const presetES2015 = require('babel-preset-es2015');
-const presetES2016 = require('babel-preset-es2016');
-const presetES2017 = require('babel-preset-es2017');
+const presetLatest = require('babel-preset-latest');
 const presetStage2 = require('babel-preset-stage-2');
-const presetStage3 = require('babel-preset-stage-3');
 const presetReact = require('babel-preset-react');
 const presetFlow = require('babel-preset-flow');
 const transformExportExtensions = require('babel-plugin-transform-export-extensions');
@@ -11,12 +8,9 @@ exports.default = function generatePreset(context, opts = {}) {
   return {
     presets: [
       opts.react !== false && presetReact,
-      opts.es2015 !== false && [presetES2015.buildPreset, opts.es2015],
-      opts.es2016 !== false && presetES2016,
-      opts.es2017 !== false && presetES2017,
       opts.flow !== false && presetFlow,
+      opts.latest !== false && [presetLatest(context, opts.latest)],
       opts.stage2 !== false && presetStage2,
-      opts.stage3 !== false && presetStage3,
     ].filter(Boolean),
     plugins: [
       transformExportExtensions,
