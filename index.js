fdcef37e40612a84bb7dda626683f62a9291fbc5
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2017-03-22 15:03:54 +1300

    v2.0.2
    - Move to stage-2/-3 presets instead of standalone plugins to avoid plugin order
      issues.

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
 const presetES2015 = require('babel-preset-es2015');
 const presetES2016 = require('babel-preset-es2016');
 const presetES2017 = require('babel-preset-es2017');
+const presetStage2 = require('babel-preset-stage-2');
+const presetStage3 = require('babel-preset-stage-3');
 const presetReact = require('babel-preset-react');
 const presetFlow = require('babel-preset-flow');
-const transformAsyncGeneratorFunctions = require('babel-plugin-transform-async-generator-functions');
-const transformClassProperties = require('babel-plugin-transform-class-properties');
 const transformExportExtensions = require('babel-plugin-transform-export-extensions');
-const transformFunctionBind = require('babel-plugin-transform-function-bind');
-const transformObjectRestSpread = require('babel-plugin-transform-object-rest-spread');
-const transformRegenerator = require('babel-plugin-transform-regenerator');
 
-module.exports = function generatePreset(context, opts = {}) {
+exports.default = function generatePreset(context, opts = {}) {
   return {
     presets: [
+      opts.react !== false && presetReact,
       opts.es2015 !== false && [presetES2015.buildPreset, opts.es2015],
       opts.es2016 !== false && presetES2016,
       opts.es2017 !== false && presetES2017,
-      opts.react !== false && presetReact,
       opts.flow !== false && presetFlow,
-    ],
+      opts.stage2 !== false && presetStage2,
+      opts.stage3 !== false && presetStage3,
+    ].filter(Boolean),
     plugins: [
-      transformAsyncGeneratorFunctions,
-      transformClassProperties,
       transformExportExtensions,
-      transformFunctionBind,
-      transformObjectRestSpread,
-      transformRegenerator,
     ],
   };
 };
 
+module.exports = exports.default;
